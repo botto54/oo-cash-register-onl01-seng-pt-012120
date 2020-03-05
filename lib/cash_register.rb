a992321d7c6a930139430eb509abbd9aa3ecde1a
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-05 01:21:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(name, price, quantity = 1)
     self.total += price * quantity
     quantity.times do
-      items << name
+      @items << name
     end
   end
   
